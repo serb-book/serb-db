1482988669bc5f5513371cffbce953c5fb30b322
Abdullah khaled <34966791+A-Siam@users.noreply.github.com>
2020-05-01 14:25:53 +0200

    update oracle web client to 8081 instade of 8080
    because tomcat will run by default on TCP:8080

--- installation.sh
+++ installation.sh
@@ -2,4 +2,4 @@ rm -rf ./serb-db >> /dev/null
 sudo docker pull wnameless/oracle-xe-11g-r2
 git clone https://github.com/serb-book/serb-db.git
 sudo docker build -t db/serb ./serb-db
-sudo docker run -p 1521:1521 -p 8080:8080 --name test_booky -e ORACLE_ALLOW_REMOTE=true db/serb
+sudo docker run -p 1521:1521 -p 8081:8080 --name test_booky -e ORACLE_ALLOW_REMOTE=true db/serb
